ec965c612ca1a51ec8671717c1f52c63423185f8
SergioGenesis97 <sergio.bautista@alumnos.udg.mx>
2026-05-07 22:45:55 -0600

    feat: implementar lectura de archivos CSV

---
+++ read_files.py
@@ -0,0 +1,18 @@
+import csv
+from pathlib import Path
+
+
+def read_file(path: str) -> dict:
+
+    # Ruta del archivo 
+    route_file = Path(path).resolve()
+
+    with open(route_file, encoding='utf-8') as file:
+        
+        reader = csv.DictReader(file)
+
+        return {row['sku']: int(row['cantidad']) for row in reader}
+        
+
+
+print(read_file('files/stock.csv'))
