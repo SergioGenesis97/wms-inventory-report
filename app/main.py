72e308c0d42c4c76699e515579907215ebf0ba2e
SergioGenesis97 <sergio.bautista@alumnos.udg.mx>
2026-05-14 22:09:27 -0600

    feat: endpoint upload CSV, fix importación y actualizar requirements

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+import csv
+import io
 from app.data_handler import read_file
 from app.services import make_report
 
@@ -18,3 +20,11 @@ def obtener_reporte_detallado():
     datos = read_file('data/stock.csv')
     txt_reporte = make_report(datos)
     return {"reporte_completo": txt_reporte}
+
+@app.post("/upload-inventory")
+async def upload_inventoy(file: UploadFile = File(...)):
+    contenido = await file.read()
+    texto = contenido.decode("utf-8")
+    reader = csv.DictReader(io.StringIO(texto))
+    inventario = {row['sku']: int(row['cantidad']) for row in reader}
+    return inventario
--- app/services.py
+++ app/services.py
@@ -1,5 +1,3 @@
-from app.services import classify_stock
-
 #Clasifica el inventario
 def classify_stock(quantity: int) -> str:
     return "Reabastecer" if quantity < 10 else ("Stock Bajo" if quantity <= 50 else "Stock Suficiente")
